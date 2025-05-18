f04b704ec7c4cfbc34b1c2aed6d3c689baae1fbe
Ok <153372447+TechWizard-007@users.noreply.github.com>
2025-05-18 17:09:32 +0530

    Charector_Animation_Setup_18may2025

--- The_School_Runner/Assets/Scripts/Player_Controller.cs
+++ The_School_Runner/Assets/Scripts/Player_Controller.cs
@@ -6,80 +6,99 @@ public class Player_Controller : MonoBehaviour
     [SerializeField] Transform left_pos;
     [SerializeField] Transform right_pos;
     [SerializeField] Rigidbody rb;
+    [SerializeField] Animator playerAnimator;
     
 
     int current_pos = 0;
     public float side_speed;
     public float runing_speed;
     public float jump_force;
+
+    bool isGameStarted = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        isGameStarted = false;
         current_pos = 0;  // 0 = center, 1 = left, 2 = right
     }
 
     // Update is called once per frame
     void Update()
-    {   
-        transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z + runing_speed*Time.deltaTime);
-        if (current_pos == 0)
+    {
+        if(!isGameStarted)
         {
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            if(Input.GetMouseButtonDown(0))
             {
-                current_pos = 1;
-            }
-            else if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                current_pos = 2;
+                Debug.Log("Game Started...");
+                isGameStarted = true;
+                //transform.forward = Vector3.forward;
+                playerAnimator.SetInteger("isRuning", 1);
+                //playerAnimator.speed = 1.2f;
             }
         }
-        else if (current_pos == 1)
-        {
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                current_pos = 0;
-            }
-        }
-        else if (current_pos == 2)
-        {
-            if(Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                current_pos = 0;
-            }
-        }
+        if (isGameStarted)
 
-        if(current_pos == 0)
-        {   
-            if(Vector3.Distance(transform.position, new Vector3(centre_pos.position.x,transform.position.y,transform.position.z)) >= 0.1f)
-            {
-                Vector3 dir = new Vector3(centre_pos.position.x, transform.position.y, transform.position.z) - transform.position;
-                transform.Translate(dir.normalized * side_speed * Time.deltaTime, Space.World);
-            }
-            
-        }
-        else if(current_pos == 1)
         {
-            if (Vector3.Distance(transform.position, new Vector3(left_pos.position.x, transform.position.y, transform.position.z)) >= 0.1f)
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + runing_speed * Time.deltaTime);
+            if (current_pos == 0)
             {
-                Vector3 dir = new Vector3(left_pos.position.x, transform.position.y, transform.position.z) - transform.position;
-                transform.Translate(dir.normalized * side_speed * Time.deltaTime, Space.World);
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    current_pos = 1;
+                }
+                else if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    current_pos = 2;
+                }
             }
-            
-        }
-        else if(current_pos == 2)
-        {
-            if (Vector3.Distance(transform.position, new Vector3(right_pos.position.x, transform.position.y, transform.position.z)) >= 0.1f)
+            else if (current_pos == 1)
             {
-                Vector3 dir = new Vector3(right_pos.position.x, transform.position.y, transform.position.z) - transform.position;
-                transform.Translate(dir.normalized * side_speed * Time.deltaTime, Space.World);
+                if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    current_pos = 0;
+                }
             }
-            
-        }
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            //rb.AddForce(Vector3.up * jump_force);
+            else if (current_pos == 2)
+            {
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    current_pos = 0;
+                }
+            }
 
-            rb.linearVelocity = Vector3.up * jump_force;
+            if (current_pos == 0)
+            {
+                if (Vector3.Distance(transform.position, new Vector3(centre_pos.position.x, transform.position.y, transform.position.z)) >= 0.1f)
+                {
+                    Vector3 dir = new Vector3(centre_pos.position.x, transform.position.y, transform.position.z) - transform.position;
+                    transform.Translate(dir.normalized * side_speed * Time.deltaTime, Space.World);
+                }
+
+            }
+            else if (current_pos == 1)
+            {
+                if (Vector3.Distance(transform.position, new Vector3(left_pos.position.x, transform.position.y, transform.position.z)) >= 0.1f)
+                {
+                    Vector3 dir = new Vector3(left_pos.position.x, transform.position.y, transform.position.z) - transform.position;
+                    transform.Translate(dir.normalized * side_speed * Time.deltaTime, Space.World);
+                }
+
+            }
+            else if (current_pos == 2)
+            {
+                if (Vector3.Distance(transform.position, new Vector3(right_pos.position.x, transform.position.y, transform.position.z)) >= 0.1f)
+                {
+                    Vector3 dir = new Vector3(right_pos.position.x, transform.position.y, transform.position.z) - transform.position;
+                    transform.Translate(dir.normalized * side_speed * Time.deltaTime, Space.World);
+                }
+
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //rb.AddForce(Vector3.up * jump_force);
+
+                rb.linearVelocity = Vector3.up * jump_force;
+            }
         }
     }
 }
